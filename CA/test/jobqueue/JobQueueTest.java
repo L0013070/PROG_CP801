03a66b61f5fb69b52a0b22871a8fda40740b00ef
L00131070 <L00131070@LYLIB2-35.lyitnet.lyit.ie>
2015-11-17 15:06:28 +0000

    added some comments

--- CA/test/jobqueue/JobQueueTest.java
+++ CA/test/jobqueue/JobQueueTest.java
@@ -12,111 +12,111 @@ import junit.framework.TestCase;
  */
 public class JobQueueTest extends TestCase {
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
 
-    /**
-     *
-     */
-    public void testJobQueue() {
-        System.out.println("testJobQueue");
-        JobQueue queue = new JobQueue();
-        assertTrue(queue != null);
-        assertTrue(queue.isRunning());
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-        System.out.println("Queue shutdown");
-        queue.setRunning(false);
-        assertTrue(!queue.isRunning());
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-        System.out.println("Queue startup");
-        queue.setRunning(true);
-        assertTrue(queue.isRunning());
-        System.out.println("Test testConstructor successful");
-    }
+	/**
+	 * Test of constructor method, of class JobQueue.
+	 * 
+	 * @throws InterruptedException
+	 *
+	 */
+	public void testJobQueue() throws InterruptedException {
+		System.out.println("testJobQueue");
+		JobQueue queue = new JobQueue();
+		assertTrue(queue != null);
+		assertTrue(queue.isRunning());
+		Thread.sleep(500);
+		System.out.println("Queue shutdown");
+		queue.setRunning(false);
+		assertTrue(!queue.isRunning());
+		Thread.sleep(500);
+		System.out.println("Queue startup");
+		queue.setRunning(true);
+		assertTrue(queue.isRunning());
+		System.out.println("Test testConstructor successful");
+	}
 
-    /**
-     *
-     */
-    public void testAdd() {
-        System.out.println("testAdd");
-        JobQueue queue = new JobQueue();
-        if (queue.add(null)) {
-            fail(" NULL JOB");
-        }
-        for (int i = 0; i < 40; i++) {
-            Job job = new TestJob();
-            boolean test = queue.add(job);
-            if (!test) {
-                assertTrue(!test && i == 20);
-                System.out.println(i + " " + test + " waiting 1s");
-                try {
-                    Thread.sleep(1000);
-                    test = queue.add(job);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        queue.setRunning(false);
-        System.out.println("Test testAdd successful");
-    }
+	/**
+	 * Test of add method, of class JobQueue.
+	 * 
+	 * @throws InterruptedException
+	 *
+	 */
+	public void testAdd() throws InterruptedException {
+		System.out.println("testAdd");
+		JobQueue queue = new JobQueue();
+		if (queue.add(null)) {
+			fail(" NULL JOB");
+		}
+		for (int i = 0; i < 40; i++) {
+			Job job = new TestJob();
+			boolean test = queue.add(job);
+			if (!test) {
+				assertTrue(!test && i == 20);
+				System.out.println(i + " " + test + " waiting 1s");
+				Thread.sleep(1000);
+				test = queue.add(job);
+			}
+		}
+		queue.setRunning(false);
+		System.out.println("Test testAdd successful");
+	}
 
-    /**
-     *
-     */
-    public void testSetRunning() {
-        System.out.println("testSetRunning");
-        JobQueue queue = new JobQueue();
-        assertTrue(queue.isRunning());
-        queue.setRunning(true);
-        assertTrue(queue.isRunning());
-        queue.setRunning(false);
-        assertTrue(!queue.isRunning());
-        queue.setRunning(false);
-        assertTrue(!queue.isRunning());
-        queue.setRunning(true);
-        assertTrue(queue.isRunning());
-        queue.setRunning(false);
-        assertTrue(!queue.isRunning());
-        System.out.println("Test testSetRunning successful");
-    }
+	/**
+	 * Test of setRunning method, of class JobQueue.
+	 *
+	 * @throws InterruptedException 
+	 *
+	 */
+	public void testSetRunning() throws InterruptedException {
+		System.out.println("testSetRunning");
+		JobQueue queue = new JobQueue();
+		assertTrue(queue.isRunning());
+		queue.setRunning(true);
+		assertTrue(queue.isRunning());
+		queue.setRunning(false);
+		assertTrue(!queue.isRunning());
+		Thread.sleep(1000);
+		queue.setRunning(false);
+		assertTrue(!queue.isRunning());
+		Thread.sleep(1000);
+		queue.setRunning(true);
+		assertTrue(queue.isRunning());
+		Thread.sleep(1000);
+		queue.setRunning(false);
+		assertTrue(!queue.isRunning());
+		System.out.println("Test testSetRunning successful");
+	}
 
-    /**
-     *
-     */
-    public void testErrorJob() {
-        System.out.println("testErrorJob");
-        JobQueue queue = new JobQueue();
-        queue.add(new TestErrorJob());
-        queue.setRunning(false);
-        System.out.println("Test testErrorJob successful");
-    }
+	/**
+	 * Test of handling exceptions thrown from doJob of the job interface in class JobQueue Thread.
+	 */
+	public void testErrorJob() {
+		System.out.println("testErrorJob");
+		JobQueue queue = new JobQueue();
+		queue.add(new TestErrorJob());
+		queue.setRunning(false);
+		System.out.println("Test testErrorJob successful");
+	}
 
-    /**
-     * Test of getWaitingThreads method, of class JobQueue.
-     */
-       public void testHandleThrowables() {
-        System.out.println("handleThrowables");
-        Throwable ex = new Exception("TestException");
-        JobQueue instance = new JobQueue();
-        instance.handleThrowables(ex);
-        System.out.println("Test testHandleThrowables successful");
-        System.err.flush();
-    }
+	/**
+	 * Test of handleThrowables method, of class JobQueue.
+	 */
+	public void testHandleThrowables() {
+		System.out.println("Test testHandleThrowables");
+		Throwable ex = new Exception("TestException");
+		JobQueue instance = new JobQueue();
+		instance.handleThrowables(ex);
+		System.err.flush();
+		System.out.println("Test testHandleThrowables successful");
+	}
 
 }
